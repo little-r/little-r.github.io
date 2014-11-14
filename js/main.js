4279e89742de2b75ece6b32fa4ce92783a355ff6
Shauna <shaunagm@gmail.com>
2014-11-13 23:22:10 -0500

    Move javascript display function to js/main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+function display(divID) {
+    var clickedDiv=document.getElementById(divID);
+    clickedDiv.style.color="#000000";
+    var pre = clickedDiv.getElementsByTagName('pre');
+    if (pre != undefined) {
+        for (i in pre) {
+            pre[i].style.color="#333";
+            pre[i].style.backgroundColor="#f5f5f5";
+            pre[i].style.border="1px solid #ccc";
+        }
+    }
 
+}
